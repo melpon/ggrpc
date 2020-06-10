e3522c2771f49f48c827c9e9094127840812439c
melpon <shigemasa7watanabe+github@gmail.com>
2020-06-10 17:02:11 +0900

    サーバストリーミングを実装

--- include/ggrpc/client.h
+++ include/ggrpc/client.h
@@ -222,6 +222,301 @@ class ClientResponseReader {
   }
 };
 
+enum class ClientReaderError {
+  CONNECT,
+  READ,
+};
+
+template <class W, class R>
+class ClientReader {
+ public:
+  typedef std::function<std::unique_ptr<grpc::ClientAsyncReader<R>>(
+      grpc::ClientContext*, const W&, grpc::CompletionQueue*, void*)>
+      ConnectFunc;
+  typedef std::function<void()> OnConnectFunc;
+  typedef std::function<void(R)> OnReadFunc;
+  typedef std::function<void(grpc::Status)> OnReadDoneFunc;
+  typedef std::function<void(ClientReaderError)> OnErrorFunc;
+
+ private:
+  struct ConnectorThunk : Handler {
+    ClientReader* p;
+    ConnectorThunk(ClientReader* p) : p(p) {}
+    void Proceed(bool ok) override { p->ProceedToConnect(ok); }
+  };
+  ConnectorThunk connector_thunk_;
+  friend class ConnectorThunk;
+
+  struct ReaderThunk : Handler {
+    ClientReader* p;
+    ReaderThunk(ClientReader* p) : p(p) {}
+    void Proceed(bool ok) override { p->ProceedToRead(ok); }
+  };
+  ReaderThunk reader_thunk_;
+  friend class ReaderThunk;
+
+  grpc::ClientContext context_;
+  std::unique_ptr<grpc::ClientAsyncReader<R>> reader_;
+
+  enum class ReadStatus {
+    INIT,
+    CONNECTING,
+    READING,
+    FINISHING,
+    CANCELING,
+    FINISHED
+  };
+  ReadStatus read_status_ = ReadStatus::INIT;
+
+  R response_;
+  grpc::Status grpc_status_;
+
+  bool release_ = false;
+  int nesting_ = 0;
+  std::mutex mutex_;
+
+  grpc::CompletionQueue* cq_;
+  ConnectFunc connect_;
+  OnConnectFunc on_connect_;
+  OnReadFunc on_read_;
+  OnReadDoneFunc on_read_done_;
+  OnErrorFunc on_error_;
+
+  struct SafeDeleter {
+    ClientReader* p;
+    std::unique_lock<std::mutex> lock;
+    SafeDeleter(ClientReader* p) : p(p), lock(p->mutex_) {}
+    ~SafeDeleter() {
+      bool del = p->release_ &&
+                 p->read_status_ == ClientReader::ReadStatus::FINISHED &&
+                 p->nesting_ == 0;
+      lock.unlock();
+      if (del) {
+        delete p;
+      }
+    }
+  };
+  friend struct SafeDeleter;
+
+  ClientReader(grpc::CompletionQueue* cq, ConnectFunc connect)
+      : connector_thunk_(this),
+        reader_thunk_(this),
+        cq_(cq),
+        connect_(std::move(connect)) {}
+
+  ~ClientReader() { SPDLOG_TRACE("[0x{}] deleted", (void*)this); }
+
+  // コピー、ムーブ禁止
+  ClientReader(const ClientReader&) = delete;
+  ClientReader(ClientReader&&) = delete;
+  ClientReader& operator=(const ClientReader&) = delete;
+  ClientReader& operator=(ClientReader&&) = delete;
+
+ public:
+  void SetOnConnect(OnConnectFunc on_connect) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (read_status_ != ReadStatus::INIT) {
+      return;
+    }
+    on_connect_ = std::move(on_connect);
+  }
+  void SetOnRead(OnReadFunc on_read) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (read_status_ != ReadStatus::INIT) {
+      return;
+    }
+    on_read_ = std::move(on_read);
+  }
+  void SetOnReadDone(OnReadDoneFunc on_read_done) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (read_status_ != ReadStatus::INIT) {
+      return;
+    }
+    on_read_done_ = std::move(on_read_done);
+  }
+  void SetOnError(OnErrorFunc on_error) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (read_status_ != ReadStatus::INIT) {
+      return;
+    }
+    on_error_ = std::move(on_error);
+  }
+
+  void Connect(const W& request) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    if (read_status_ != ReadStatus::INIT) {
+      return;
+    }
+    read_status_ = ReadStatus::CONNECTING;
+    reader_ = connect_(&context_, request, cq_, &connector_thunk_);
+  }
+
+ private:
+  void Release() {
+    SafeDeleter d(this);
+
+    release_ = true;
+
+    DoClose(d.lock);
+  }
+  friend class ClientManager;
+
+ public:
+  void Close() {
+    SafeDeleter d(this);
+
+    DoClose(d.lock);
+  }
+
+ private:
+  void DoClose(std::unique_lock<std::mutex>& lock) {
+    // 読み込み中だったらキャンセルされるまで待つ
+    if (read_status_ == ReadStatus::CONNECTING ||
+        read_status_ == ReadStatus::READING ||
+        read_status_ == ReadStatus::FINISHING) {
+      context_.TryCancel();
+    }
+    if (read_status_ == ReadStatus::CONNECTING ||
+        read_status_ == ReadStatus::READING ||
+        read_status_ == ReadStatus::FINISHING) {
+      read_status_ = ReadStatus::CANCELING;
+    } else {
+      read_status_ = ReadStatus::FINISHED;
+    }
+    Done(lock);
+  }
+
+  void Done(std::unique_lock<std::mutex>& lock) {
+    if (read_status_ != ReadStatus::FINISHED) {
+      return;
+    }
+
+    auto on_connect = std::move(on_connect_);
+    auto on_read = std::move(on_read_);
+    auto on_read_done = std::move(on_read_done_);
+    auto on_error = std::move(on_error_);
+
+    ++nesting_;
+    lock.unlock();
+    on_connect = nullptr;
+    on_read = nullptr;
+    on_read_done = nullptr;
+    on_error = nullptr;
+    lock.lock();
+    --nesting_;
+  }
+
+ private:
+  template <class F, class... Args>
+  void RunCallback(std::unique_lock<std::mutex>& lock, std::string funcname,
+                   F f, Args&&... args) {
+    // 普通にコールバックするとデッドロックの可能性があるので
+    // unlock してからコールバックする。
+    // 再度ロックした時に状態が変わってる可能性があるので注意すること。
+    if (f) {
+      ++nesting_;
+      lock.unlock();
+      try {
+        SPDLOG_TRACE("call {}", funcname);
+        f(std::forward<Args>(args)...);
+      } catch (std::exception& e) {
+        SPDLOG_ERROR("{} error: what={}", funcname, e.what());
+      } catch (...) {
+        SPDLOG_ERROR("{} error", funcname);
+      }
+      f = nullptr;
+      lock.lock();
+      --nesting_;
+    }
+  }
+
+  void ProceedToConnect(bool ok) {
+    SafeDeleter d(this);
+    SPDLOG_TRACE("[0x{}] ProceedToConnect: ok={}", (void*)this, ok);
+
+    // read が CONNECTING か CANCELING になることしか無いはず
+    assert(read_status_ == ReadStatus::CONNECTING ||
+           read_status_ == ReadStatus::CANCELING);
+
+    if (read_status_ != ReadStatus::CONNECTING) {
+      // 既に Close が呼ばれてるので終わる
+      Done(d.lock);
+      return;
+    }
+
+    // 接続失敗
+    if (!ok) {
+      SPDLOG_ERROR("connection error");
+
+      read_status_ = ReadStatus::FINISHED;
+      RunCallback(d.lock, "OnError", on_error_, ClientReaderError::CONNECT);
+      Done(d.lock);
+      return;
+    }
+
+    // 読み込み
+    reader_->Read(&response_, &reader_thunk_);
+    read_status_ = ReadStatus::READING;
+
+    RunCallback(d.lock, "OnConnect", on_connect_);
+  }
+
+  void ProceedToRead(bool ok) {
+    SafeDeleter d(this);
+    SPDLOG_TRACE("[0x{}] ProceedToRead: ok={}", (void*)this, ok);
+
+    assert(read_status_ == ReadStatus::READING ||
+           read_status_ == ReadStatus::FINISHING ||
+           read_status_ == ReadStatus::CANCELING);
+
+    if (read_status_ == ReadStatus::CANCELING) {
+      read_status_ = ReadStatus::FINISHED;
+      Done(d.lock);
+      return;
+    }
+
+    if (!ok) {
+      if (read_status_ == ReadStatus::READING) {
+        // 正常に読み込み完了した可能性があるので Finish する
+        reader_->Finish(&grpc_status_, &reader_thunk_);
+        read_status_ = ReadStatus::FINISHING;
+      } else if (read_status_ == ReadStatus::FINISHING) {
+        SPDLOG_ERROR("reading or finishing error");
+        read_status_ = ReadStatus::FINISHED;
+        RunCallback(d.lock, "OnError", on_error_, ClientReaderError::READ);
+        Done(d.lock);
+      }
+      return;
+    }
+
+    if (read_status_ == ReadStatus::READING) {
+      // 結果が取得できた
+      auto resp = std::move(response_);
+
+      // 次の読み込み
+      reader_->Read(&response_, &reader_thunk_);
+      read_status_ = ReadStatus::READING;
+
+      // 読み込み成功コールバック
+      RunCallback(d.lock, "OnRead", on_read_, std::move(resp));
+    } else if (read_status_ == ReadStatus::FINISHING) {
+      // 終了
+
+      if (grpc_status_.ok()) {
+        SPDLOG_DEBUG("gRPC Read finished");
+      } else {
+        SPDLOG_ERROR("gRPC error: {} ({})", grpc_status_.error_message(),
+                     grpc_status_.error_code());
+        SPDLOG_ERROR("   details: {}", grpc_status_.error_details());
+      }
+      // 正常終了
+      read_status_ = ReadStatus::FINISHED;
+      RunCallback(d.lock, "OnReadDone", on_read_done_, grpc_status_);
+      Done(d.lock);
+    }
+  }
+};
+
 enum class ClientReaderWriterError {
   CONNECT,
   READ,
@@ -463,6 +758,7 @@ class ClientReaderWriter {
     auto on_connect = std::move(on_connect_);
     auto on_read = std::move(on_read_);
     auto on_read_done = std::move(on_read_done_);
+    auto on_write = std::move(on_write_);
     auto on_writes_done = std::move(on_writes_done_);
     auto on_error = std::move(on_error_);
 
@@ -471,7 +767,7 @@ class ClientReaderWriter {
     on_connect = nullptr;
     on_read = nullptr;
     on_read_done = nullptr;
-    on_write_ = nullptr;
+    on_write = nullptr;
     on_writes_done = nullptr;
     on_error = nullptr;
     lock.lock();
@@ -728,6 +1024,18 @@ class ClientManager {
     bool Expired() override { return wp.expired(); }
   };
   template <class W, class R>
+  struct ReaderHolder : Holder {
+    std::weak_ptr<ClientReader<W, R>> wp;
+    ReaderHolder(std::shared_ptr<ClientReader<W, R>> p) : wp(p) {}
+    void Close() override {
+      auto sp = wp.lock();
+      if (sp) {
+        sp->Close();
+      }
+    }
+    bool Expired() override { return wp.expired(); }
+  };
+  template <class W, class R>
   struct ReaderWriterHolder : Holder {
     std::weak_ptr<ClientReaderWriter<W, R>> wp;
     ReaderWriterHolder(std::shared_ptr<ClientReaderWriter<W, R>> p) : wp(p) {}
@@ -832,6 +1140,23 @@ class ClientManager {
         [](ClientResponseReader<W, R>* p) { p->Release(); });
     holders_.push_back(
         std::unique_ptr<Holder>(new ResponseReaderHolder<W, R>(p)));
+    return p;
+  }
+
+  template <class W, class R>
+  std::shared_ptr<ClientReader<W, R>> CreateReader(
+      typename ClientReader<W, R>::ConnectFunc connect) {
+    std::lock_guard<std::mutex> guard(mutex_);
+
+    Collect();
+
+    auto client_id = next_client_id_++;
+    auto cq = &threads_[client_id % threads_.size()].cq;
+
+    std::shared_ptr<ClientReader<W, R>> p(
+        new ClientReader<W, R>(cq, std::move(connect)),
+        [](ClientReader<W, R>* p) { p->Release(); });
+    holders_.push_back(std::unique_ptr<Holder>(new ReaderHolder<W, R>(p)));
     return p;
   }
 
--- test/ggrpc_test.cpp
+++ test/ggrpc_test.cpp
@@ -42,6 +42,30 @@ class TestUnaryHandler
   void OnError(ggrpc::ServerResponseWriterError error) override {}
 };
 
+class TestSstreamHandler
+    : public ggrpc::ServerWriterHandler<gg::SstreamResponse,
+                                        gg::SstreamRequest> {
+  gg::Test::AsyncService* service_;
+
+ public:
+  TestSstreamHandler(gg::Test::AsyncService* service) : service_(service) {}
+  void Request(grpc::ServerContext* context, gg::SstreamRequest* request,
+               grpc::ServerAsyncWriter<gg::SstreamResponse>* writer,
+               grpc::ServerCompletionQueue* cq, void* tag) override {
+    service_->RequestSstream(context, request, writer, cq, cq, tag);
+  }
+  void OnAccept(gg::SstreamRequest request) override {
+    SPDLOG_TRACE("received SstreamRequest: {}", request.DebugString());
+    gg::SstreamResponse resp;
+    resp.set_value(request.value() * 1);
+    Context()->Write(resp);
+    resp.set_value(request.value() * 2);
+    Context()->Write(resp);
+    Context()->Finish(grpc::Status::OK);
+  }
+  void OnError(ggrpc::ServerWriterError error) override {}
+};
+
 class TestBidiHandler
     : public ggrpc::ServerReaderWriterHandler<gg::BidiResponse,
                                               gg::BidiRequest> {
@@ -90,8 +114,9 @@ class TestServer {
     SPDLOG_INFO("gRPC Server listening on {}", address);
 
     // ハンドラの登録
-    server_.AddReaderWriterHandler<TestBidiHandler>(&service_);
     server_.AddResponseWriterHandler<TestUnaryHandler>(&service_);
+    server_.AddWriterHandler<TestSstreamHandler>(&service_);
+    server_.AddReaderWriterHandler<TestBidiHandler>(&service_);
 
     server_.Start(builder, threads);
   }
@@ -99,6 +124,8 @@ class TestServer {
 
 typedef ggrpc::ClientResponseReader<gg::UnaryRequest, gg::UnaryResponse>
     UnaryClient;
+typedef ggrpc::ClientReader<gg::SstreamRequest, gg::SstreamResponse>
+    SstreamClient;
 typedef ggrpc::ClientReaderWriter<gg::BidiRequest, gg::BidiResponse> BidiClient;
 
 class TestClientManager {
@@ -118,6 +145,15 @@ class TestClientManager {
                              const gg::UnaryRequest& request,
                              grpc::CompletionQueue* cq) {
           return stub->AsyncUnary(context, request, cq);
+        });
+  }
+
+  std::shared_ptr<SstreamClient> CreateSstream() {
+    return cm_.CreateReader<gg::SstreamRequest, gg::SstreamResponse>(
+        [stub = stub_.get()](grpc::ClientContext* context,
+                             const gg::SstreamRequest& request,
+                             grpc::CompletionQueue* cq, void* tag) {
+          return stub->AsyncSstream(context, request, cq, tag);
         });
   }
 
@@ -238,6 +274,37 @@ void test_client_unary() {
     unary->SetOnResponse(
         [unary](gg::UnaryResponse resp, grpc::Status) { unary->Close(); });
     unary->Request(req);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+}
+
+void test_client_sstream() {
+  TestServer server;
+  server.Start("0.0.0.0:50051", 10);
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  auto channel = grpc::CreateChannel("localhost:50051",
+                                     grpc::InsecureChannelCredentials());
+  TestClientManager cm(channel, 10);
+  cm.Start();
+
+  gg::SstreamRequest req;
+
+  {
+    auto ss = cm.CreateSstream();
+    req.set_value(100);
+    int n = 0;
+    ss->SetOnRead([&n](gg::SstreamResponse resp) { n += resp.value(); });
+    ss->Connect(req);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    ASSERT(n == 300);
+  }
+
+  {
+    auto ss = cm.CreateSstream();
+    req.set_value(100);
+    ss->SetOnRead([ss](gg::SstreamResponse resp) { ss->Close(); });
+    ss->Connect(req);
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
@@ -367,61 +434,8 @@ int main() {
 
   test_client_bidi_connect_callback();
   test_client_unary();
+  test_client_sstream();
   test_server();
   test_client_alarm();
   test_server_alarm();
-
-  //std::unique_ptr<TestServer> server(new TestServer());
-  //server->Start("0.0.0.0:50051", 10);
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  //auto channel = grpc::CreateChannel("localhost:50051",
-  //                                   grpc::InsecureChannelCredentials());
-  //std::unique_ptr<ClientManager> cm(new ClientManager(channel, 10));
-  //auto bidi = cm->TestBidi(
-  //    []() { SPDLOG_TRACE("connected BidiTest"); },
-  //    [](gg::BidiResponse resp) {
-  //      SPDLOG_TRACE("received BidiResponse {}", resp.DebugString());
-  //    },
-  //    [](grpc::Status status) {
-  //      SPDLOG_TRACE("gRPC Error: {}", status.error_message());
-  //    },
-  //    [](ggrpc::ClientReaderWriterError error) {
-  //      SPDLOG_TRACE("ClientReaderWriterError: {}", (int)error);
-  //    });
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-  //gg::BidiRequest req;
-  //req.set_value(100);
-  //bidi->Write(req);
-  //bidi->WritesDone();
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  //auto alarm = cm->CreateAlarm(std::chrono::milliseconds(100),
-  //                             []() { SPDLOG_TRACE("Alarm"); });
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  //alarm = cm->CreateAlarm(std::chrono::milliseconds(100), [&]() {
-  //  SPDLOG_TRACE("Alarm 2");
-  //  alarm = cm->CreateAlarm(std::chrono::milliseconds(100),
-  //                          []() { SPDLOG_TRACE("Alarm 3"); });
-  //});
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  //// 即座にキャンセルする
-  //alarm = cm->CreateAlarm(std::chrono::milliseconds(100),
-  //                        [&]() { SPDLOG_ERROR("Alarm 4"); });
-  //alarm.reset();
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
-
-  //gg::UnaryRequest unary_req;
-  //unary_req.set_value(1);
-  //auto unary = cm->TestUnary(
-  //    unary_req,
-  //    [](gg::UnaryResponse resp, grpc::Status status) {
-  //      SPDLOG_TRACE("received UnaryResponse {}", resp.DebugString());
-  //    },
-  //    [](ggrpc::ClientResponseReaderError error) {
-  //      SPDLOG_TRACE("ClientResponseReaderError: {}", (int)error);
-  //    });
-  //std::this_thread::sleep_for(std::chrono::seconds(1));
 }
